9fac91851b304da0f8e3ef7734725b39be332f8f
HuyNguyen1977 <huycnn@gmail.com>
2021-12-31 14:43:16 +0700

    change code 30122021

--- base/views.py
+++ base/views.py
@@ -91,19 +91,19 @@ def room(request, pk):
     participants = room.participants.all()
     participantsUser=[]
     # Get thanh vien tham gia
-    for userPa in participants:
-        participantsUser.append(userPa.username)
-        print((participantsUser))
+    # for userPa in participants:
+    #     participantsUser.append(userPa.username)
+    #     print((participantsUser))
     #end Get thanh vien tham gia   
     # print(vars(participants))
-    if request.user.username not in participantsUser and request.user.username != room.host.username :
-        # print("ton tai")
-        # context = { 'message':'Bạn không có quyền trong phòng này','permit':'notpermit'}
-        messages.warning(request, 'Bạn không có quyền tham gia')
-        return redirect('home')
+    # if request.user.username not in participantsUser and request.user.username != room.host.username :
+    #     # print("ton tai")
+    #     # context = { 'message':'Bạn không có quyền trong phòng này','permit':'notpermit'}
+    #     messages.warning(request, 'Bạn không có quyền tham gia')
+    #     return redirect('home')
     # print(request.user.username)
-    # print(vars(room.host))
-    if request.method == 'POST':
+    # print(vars(room))
+    if request.method == 'POST' and request.user.is_authenticated:
         message = Message.objects.create(
             user=request.user,
             room=room,
