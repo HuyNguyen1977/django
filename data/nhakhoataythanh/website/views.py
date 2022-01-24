336a52e5491262c6dafd5ab1b381428a4d818ffa
HuyNguyen1977 <huycnn@gmail.com>
2022-01-24 16:21:57 +0700

    taythanh

--- data/nhakhoataythanh/website/views.py
+++ data/nhakhoataythanh/website/views.py
@@ -6,7 +6,35 @@ import socket
 
 
 def home(request):
-    return render(request, 'website/home.html')
+    if request.method == 'POST':
+        time = request.POST['home_time']
+        scheldule = request.POST['home_scheldule']
+        address = request.POST['home_address']
+        name = request.POST['home_name']
+        phone = request.POST['home_phone']
+        email = request.POST['home_email']
+        message = request.POST['home_message']
+        home_text_all = 'Tên : '+name+'<br>Thời gian : '+time+'<br>Lịch : '+scheldule+'<br>Địa chỉ : '+address+'<br>Phone : '+phone+'<br>email : '+email+'<br>Nội dung : '+message
+        # socket.getaddrinfo('gmail.com', 80)
+        # #send email to default address
+        # send_mail(
+        #     'Follow up required for - ' + name,
+        #     message,
+        #     email,
+        #     [conf_settings.CONTACT_US_FORM_EMAIL_TO],
+        #     fail_silently=False,
+        # )
+        res = sm(
+                subject = '[Khách hàng]Booking',
+                message = home_text_all,
+                from_email = email,
+                recipient_list = ['huynguyen@saigonbooks.vn'],
+                fail_silently=False,
+            )    
+        messages.success(request, f'Hi {name}, Thanks for contacting us. We will follow up with you within next few business days.')
+        return redirect('/')
+    else:
+        return render(request, 'website/home.html')
 
 def about(request):
     return render(request, 'website/about.html')
